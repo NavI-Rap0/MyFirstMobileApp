ef0883bbb12da35b47acd0229f16d94f10156aa9
Ivan <vanya.opar@gmail.com>
2024-12-09 17:36:09 +0200

    1screen, add event and add todo logic 20%

--- App.tsx
+++ App.tsx
@@ -1,118 +1,79 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MainMenuScreen from './src/screens/MainMenuScreen';
+import HomeScreen from './src/screens/HomeScreen';
+import TodoListScreen from './src/screens/TodoListScreen';
+import CreateTodoScreen from './src/screens/CreateTodoScreen';
+import HeaderLeftButton from './src/components/HeaderLeftButton';
+import CreateEventScreen from './src/screens/CreateEventScreen';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Stack = createNativeStackNavigator();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const App: React.FC = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="MainMenu">
+        {/* Main Menu Screen */}
+        <Stack.Screen
+          name="MainMenu"
+          component={MainMenuScreen}
+          options={{ headerShown: false }}
+        />
+
+        {/* Home Screen */}
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerShown: false, // Прибирає заголовок
+          }}
+        />
+
+        {/* TodoList1 Screen */}
+        <Stack.Screen
+          name="TodoList1"
+          options={{
+            headerLeft: () => <HeaderLeftButton />,
+          }}
+        >
+          {(props) => <TodoListScreen {...props} listId={1} />}
+        </Stack.Screen>
+
+        {/* TodoList2 Screen */}
+        <Stack.Screen
+          name="TodoList2"
+          options={{
+            headerLeft: () => <HeaderLeftButton />,
+          }}
+        >
+          {(props) => <TodoListScreen {...props} listId={2} />}
+        </Stack.Screen>
+
+        {/* CreateTodo1 Screen */}
+        <Stack.Screen
+          name="CreateTodo1"
+          options={{
+            headerLeft: () => <HeaderLeftButton />,
+          }}
+          component={CreateTodoScreen}
+        />
+
+        {/* CreateTodo2 Screen */}
+        <Stack.Screen
+          name="CreateTodo2"
+          options={{
+            headerLeft: () => <HeaderLeftButton />,
+          }}
+          component={CreateTodoScreen}
+        />
+        <Stack.Screen name="CreateEvent" component={CreateEventScreen} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
+
+
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,15 @@
+
 module.exports = {
+  setupFiles: ['<rootDir>/jest-setup.js'],
+
   preset: 'react-native',
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(png|jpg|jpeg|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(@react-navigation|react-native|react-native-reanimated))',
+  ],
 };
